ddf53d50c25b19eaeb056c2f33970da52ee3200b
Wei Peluso <weipeluso@MacBook-Pro.local>
2020-07-07 16:37:16 -0400

    finished get posts

---
+++ posts/posts-router.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const Posts = require("../data/db.js");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Posts.find(req.query)
+    .then((posts) => {
+      res.status(200).json({ query: req.query, data: posts });
+    })
+    .catch((error) => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving the posts",
+      });
+    });
+});
+
+module.exports = router;
